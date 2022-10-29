3619caef04a0924dea8b3a14c39245304f738bbc
Carlos Daniel de Lira Salas <99099769+CDanielLira@users.noreply.github.com>
2022-10-29 16:35:52 -0500

    Add Form to login

--- formlog.php
+++ formlog.php
@@ -1,34 +1,40 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
+
 <body>
     <?php
         include("header.html");
     ?>
-    <br>
-    <br>
-    <form>
-        <div class="form-group">
-            <label for="exampleInputEmail1">Usuario</label>
-            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-            <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-        </div>
-        <div class="form-group">
-            <label for="exampleInputPassword1">Contraseña</label>
-            <input type="password" class="form-control" id="exampleInputPassword1">
-        </div>
-        <div class="form-group form-check">
-            <input type="checkbox" class="form-check-input" id="exampleCheck1">
-            <label class="form-check-label" for="exampleCheck1">Recuerdame</label>
-        </div>
-        <button type="submit" class="btn btn-primary">Iniciar Sesión</button>
-    </form>
+    <br><br><br>
+    <div class="form">
+       <fieldset form>
+          <legend>Inicio de sesión</legend>
+           <form action="login.php" method="post">
+            <div class="form-group">
+                <label for="exampleInputEmail1">Usuario</label>
+                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name="usuario">
+                <small id="emailHelp" class="form-text text-muted">La información proporcionada es solo tuya.</small>
+            </div>
+            <div class="form-group">
+                <label for="exampleInputPassword1">Contraseña</label>
+                <input type="password" class="form-control" id="exampleInputPassword1" name="contra">
+            </div>
+            <div class="form-group">
+                <a href="formreg.php"><label class="form-check-label" for="exampleCheck1">¿Aún no tienes cuenta?</label></a>
+            </div>
+            <button type="submit" class="btn btn-primary">Iniciar Sesión</button>
+        </form>
+       </fieldset>
+    </div>
     <?php
         include("Footer.html");
     ?>
 </body>
+
 </html>
