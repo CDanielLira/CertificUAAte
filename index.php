763c9e545b9a4a8e4ffe93f357dbe0ce627d55da
LEPEG0 <116929271+LEPEG0@users.noreply.github.com>
2022-10-29 20:29:42 -0500

    Agregar Login

--- index.php
+++ index.php
@@ -161,7 +161,7 @@
                 </div>
             </section>
             <?php
-                include ("header.html");
+                include ("Footer.html");
             ?>
         </main>
         <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
