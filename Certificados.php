09396ecba74b9ab017b131f8c726bbdc971295d2
Deivis9090 <116929069+Deivis9090@users.noreply.github.com>
2022-10-30 09:30:59 -0600

    Agregar archivo de Pago.php y sus Estilos, conectar con Certificados para poder pagar el examen...

--- Certificados.php
+++ Certificados.php
@@ -15,7 +15,7 @@
                 <p>Al estar lo mas actualizado posible en este lenguaje, garantizas que tienes una base solida de conocimentos logicos que te respalda</p>
                 <p>Consta de 8 simples preguntas en un tiempo de 10 min en el que para aprobar deberas contestar correctamente al menos cinco de ellas.</p>
                 <p>El costo es de 50 pesos y esta disponible siempre que se quiera aplicar.</p>
-                <a href="#" class="btn btn-primary btn-lg active" role="button" aria-pressed="true">Comenzar</a>
+                <a href="Pago.php" class="btn btn-primary btn-lg active" role="button" aria-pressed="true">Comenzar</a>
             </div>
         </div>
     </div>
@@ -27,7 +27,7 @@
                     Con los conocimientos impartidos por Netec usted podrá aportar rendimiento, seguridad y estabilidad a las aplicaciones, podrá además minimizar costos de implementación, monitoreo y mantenimiento</p>
                 <p>Consta de 8 simples preguntas en un tiempo de 10 min en el que para aprobar deberas contestar correctamente al menos cinco de ellas.</p>
                 <p>El costo es de 50 pesos y esta disponible siempre que se quiera aplicar.</p>
-                <a href="#" class="btn btn-primary btn-lg active" role="button" aria-pressed="true">Comenzar</a>
+                <a href="Pago.php" class="btn btn-primary btn-lg active" role="button" aria-pressed="true">Comenzar</a>
             </div>
             <div class="image4">
                 <img src="img/undraw_JavaScript_frameworks_8qpc.png" alt="" width="600">
@@ -43,7 +43,7 @@
                     En esta Certificación de Diseño Web Responsivo, aprenderás los lenguajes que los desarrolladores utilizan para construir páginas web: HTML (lenguaje de marcado de hipertexto) para el contenido</p>
                 <p>Consta de 8 simples preguntas en un tiempo de 10 min en el que para aprobar deberas contestar correctamente al menos cinco de ellas.</p>
                 <p>El costo es de 50 pesos y esta disponible siempre que se quiera aplicar.</p>
-                <a href="#" class="btn btn-primary btn-lg active" role="button" aria-pressed="true">Comenzar</a>
+                <a href="Pago.php" class="btn btn-primary btn-lg active" role="button" aria-pressed="true">Comenzar</a>
             </div>
         </div>
     </div>
@@ -55,7 +55,7 @@
                     Con este curso de CSS3 aprenderás a darle un aspecto único, personalizado y más atractivo tus páginas web por medio de la creación de animaciones y de tus propios efectos interactivos</p>
                 <p>Consta de 8 simples preguntas en un tiempo de 10 min en el que para aprobar deberas contestar correctamente al menos cinco de ellas.</p>
                 <p>El costo es de 50 pesos y esta disponible siempre que se quiera aplicar.</p>
-                <a href="#" class="btn btn-primary btn-lg active" role="button" aria-pressed="true">Comenzar</a>
+                <a href="Pago.php" class="btn btn-primary btn-lg active" role="button" aria-pressed="true">Comenzar</a>
             </div>
             <div class="image4">
                 <img src="img/undraw_static_assets_rpm6.png" alt="" width="600">
@@ -64,6 +64,8 @@
     </div>
     <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
+    
+    <header>
     <?php
             include ("header.html");
         ?>
@@ -71,6 +73,7 @@
     <?php
         include ("Footer.html");
     ?>
+    
 </body>
 
 </html>
---
+++ Pago.php
@@ -0,0 +1,134 @@
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Pago de Curso</title>
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="EstilosPago.css">
+
+</head>
+
+<body>
+
+    <div class="Bloque">
+
+        <h2> - PAGO DEL CURSO - </h2>
+
+        <form action="">
+
+            <table class="Cuerpo">
+
+                <tr>
+
+                    <td class="Datos">
+                        <div class="form-group">
+                            <label for="Nombre"> Número de Tarjeta: </label>
+                            <input type="number" class="form-control" max="9999999999999999" id="NumTajeta" name="NumTarjeta">
+                        </div>
+                    </td>
+
+                    <br><br>
+
+                    <td class="Datos">
+                        <div class="form-group">
+                            <label for="Banco"> Banco: </label>
+                            <select class="form-control" id="Banco" name="Banco">
+                                <option value="BBVA">BBVA</option>
+                                <option value="SANT">SANTANDER</option>
+                                <option value="HSBC">HSBC</option>
+                                <option value="CITI">CITIBANAMEX</option>
+                                <option value="BANO">BANORTE</option>
+                                <option value="SCOT">SCOTIABANK</option>
+                            </select>
+                        </div>
+                    </td>
+
+                </tr>
+
+                <tr>
+
+                    <td class="Datos">
+                        <div class="form-group">
+
+                            <label for="Banco"> Fecha de Vencimiento: </label>
+                            <input type="month" class="form-control" id="FechaVen" name="FechaVen">
+
+                        </div>
+                    </td>
+
+                    <td class="Datos">
+
+                        <div class="form-group">
+
+                            <label for="Pass">Código de Verificación: </label><br>
+                            <input type="password" maxlength="3" class="form-control" id="CodVer" name="CodVer">
+
+                        </div>
+
+                    </td>
+                </tr>
+
+                <tr>
+
+                    <td colspan="2" style=" padding-right: 1500px;">
+
+                        <div class="form-group">
+
+                            <input type="submit" class="form-control" value="Enviar">
+
+                        </div>
+
+                    </td>
+
+
+                </tr>
+
+            </table>
+
+        </form>
+
+    </div>
+
+    <header>
+        <?php
+            include ("header.html");
+        ?>
+    </header>
+    <?php
+        include ("Footer.html");
+    ?>
+
+</body>
+
+</html>
+
+<?php 
+
+    // VARIABLES DEL FORMULARIO (DECLARACION EN PHP) ---------------------------------------------------------------------------------
+
+    $BANCO = $FECHA_VEN = $COD_VER = ""; 
+    $NUM_TARJETA = 0;
+
+// FUNCIONES DE FILTRADO E INGRESO DE DATOS --------------------------------------------------------------------------------------
+
+    function Filtrado($Datos) {
+
+        $Datos = trim($Datos); // Elimina espacios antes y después de los datos
+        $Datos = stripslashes($Datos); // Elimina backslashes \
+        $Datos = htmlspecialchars($Datos); // Traduce caracteres especiales en entidades HTML
+        return $Datos;
+    }
+
+    if (isset($_POST["submit"]) && $_SERVER["REQUEST_METHOD"] == "POST") {
+
+        $NUM_TARJETA = Filtrado($_POST["NumTarjeta"]);
+        $BANCO = Filtrado($_POST["Banco"]);
+        $FECHA_VEN = Filtrado($_POST["FechaVen"]);
+        $COD_VER = Filtrado($_POST["CodVer"]);
+    }
+
+    
+
+?>
