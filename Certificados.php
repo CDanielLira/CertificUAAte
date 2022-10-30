8b4a1fc2e00bc216e2fd62b07665731e4ab5bd2a
Carcio03 <116929433+Carcio03@users.noreply.github.com>
2022-10-30 08:34:34 -0600

    Pagina de info de certificados

---
+++ Certificados.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+    <link rel="stylesheet" href="style3.css">
+</head>
+
+<body>
+    <div class="certificado1">
+        <div class="grid1">
+            <div class="texto">
+                <h3>Certificacion de C#</h3>
+                <p>Al estar lo mas actualizado posible en este lenguaje, garantizas que tienes una base solida de conocimentos logicos que te respalda</p>
+                <p>Consta de 8 simples preguntas en un tiempo de 10 min en el que para aprobar deberas contestar correctamente al menos cinco de ellas.</p>
+                <p>El costo es de 50 pesos y esta disponible siempre que se quiera aplicar.</p>
+                <a href="#" class="btn btn-primary btn-lg active" role="button" aria-pressed="true">Comenzar</a>
+            </div>
+        </div>
+    </div>
+    <div class="certificado2">
+        <div class="grid2">
+            <div class="texto">
+                <h3>Certificacion de JAVA</h3>
+                <p>Java, es utilizado desde móviles hasta electrodomésticos por lo que se convierte en uno de los lenguajes más utilizados en la actualidad a nivel mundial. Conocer y manejar este lenguaje informático te permitirá crear aplicaciones y procesos en una diversa gama de dispositivos.
+                    Con los conocimientos impartidos por Netec usted podrá aportar rendimiento, seguridad y estabilidad a las aplicaciones, podrá además minimizar costos de implementación, monitoreo y mantenimiento</p>
+                <p>Consta de 8 simples preguntas en un tiempo de 10 min en el que para aprobar deberas contestar correctamente al menos cinco de ellas.</p>
+                <p>El costo es de 50 pesos y esta disponible siempre que se quiera aplicar.</p>
+                <a href="#" class="btn btn-primary btn-lg active" role="button" aria-pressed="true">Comenzar</a>
+            </div>
+            <div class="image4">
+                <img src="img/undraw_JavaScript_frameworks_8qpc.png" alt="" width="600">
+            </div>
+
+        </div>
+    </div>
+    <div class="certificado3">
+        <div class="grid3">
+            <div class="texto">
+                <h3>Certificacion de HTML</h3>
+                <p>HTML es un lenguaje de marcado que usa una sintaxis o notación especial para describir la estructura de una página web al navegador. Los elementos HTML generalmente tienen etiquetas de apertura y cierre que rodean y dan sentido al contenido.
+                    En esta Certificación de Diseño Web Responsivo, aprenderás los lenguajes que los desarrolladores utilizan para construir páginas web: HTML (lenguaje de marcado de hipertexto) para el contenido</p>
+                <p>Consta de 8 simples preguntas en un tiempo de 10 min en el que para aprobar deberas contestar correctamente al menos cinco de ellas.</p>
+                <p>El costo es de 50 pesos y esta disponible siempre que se quiera aplicar.</p>
+                <a href="#" class="btn btn-primary btn-lg active" role="button" aria-pressed="true">Comenzar</a>
+            </div>
+        </div>
+    </div>
+    <div class="certificado4">
+        <div class="grid4">
+            <div class="texto">
+                <h3>Certificacion de CSS</h3>
+                <p>El conocimiento es poder, especialmente en el mercado de trabajo actual. Documentación de sus habilidades le permite avanzar en su carrera, o ayudarle a comenzar una nueva.
+                    Con este curso de CSS3 aprenderás a darle un aspecto único, personalizado y más atractivo tus páginas web por medio de la creación de animaciones y de tus propios efectos interactivos</p>
+                <p>Consta de 8 simples preguntas en un tiempo de 10 min en el que para aprobar deberas contestar correctamente al menos cinco de ellas.</p>
+                <p>El costo es de 50 pesos y esta disponible siempre que se quiera aplicar.</p>
+                <a href="#" class="btn btn-primary btn-lg active" role="button" aria-pressed="true">Comenzar</a>
+            </div>
+            <div class="image4">
+                <img src="img/undraw_static_assets_rpm6.png" alt="" width="600">
+            </div>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
+    <?php
+            include ("header.html");
+        ?>
+    </header>
+    <?php
+        include ("Footer.html");
+    ?>
+</body>
+
+</html>
