07555263b13f3375157f7e571a2944bd99e7ad3c
LEPEG0 <116929271+LEPEG0@users.noreply.github.com>
2022-10-29 20:09:29 -0500

    Agrgar login

--- index.php
+++ index.php
@@ -11,6 +11,12 @@
     <script src="https://kit.fontawesome.com/bf93869655.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="style.css" />
     <link rel="stylesheet" href="style2.css">
+    <style>
+        [class^="fa"] {
+            font-size: 15px;
+            text-align: center;
+        }
+    </style>
 </head>
 
 <body>
@@ -160,6 +166,9 @@
                     <img src="img/undraw_Code_thinking_re_gka2.png" alt="" class="img">
                 </div>
             </section>
+            <?php
+                include ("header.html");
+            ?>
         </main>
         <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
