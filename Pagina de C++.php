7acb9518a2894fb1cdb1576e79e17a71b3aa3559
LEPEG0 <116929271+LEPEG0@users.noreply.github.com>
2022-10-30 18:32:43 -0600

    PAGINAS

---
+++ Pagina de C++.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+    <style>
+        h5,
+        p {
+            color: black
+        }
+
+        .card {
+            border: solid black 3px;
+        }
+
+        body {
+            background: #000000;
+            background: -webkit-linear-gradient(to left, #434343, #000000);
+            background: linear-gradient(to left, #434343, #000000);
+        }
+
+    </style>
+</head>
+
+<body>
+    <div class="card bg-dark text-white" style="margin: 10px;">
+        <img src="img/C++.png" class="card-img" alt="..." height="350px">
+        <div class="card-img-overlay" style="border: solid black 3px;">
+            <h5 class="card-title" style="text-align: center">CONSIGUE TU CERTIFICADO DE C++ AQUI</h5>
+        </div>
+    </div>
+    <div>
+        <div class="card" style="width: 18rem; text-align: center; margin: 0 auto;">
+            <img src="img/-blog-que-es-cpp-.jpg" class="card-img-top" alt="..." height="200px" width="200px">
+            <div class="card-body">
+                <p class="card-text">AL HACER ESTE EXAMEN SOLO SE PODRA HACER UNA SOLA VEZ, SOLO SON 8 PREGUNTAS ASI QUE MUCHA SUERTE</p>
+            </div>
+            <img src="img/430659_8969215_updates.jpg" class="card-img-top" alt="..." height="200px" width="200px">
+            <div class="card-body">
+                <p class="card-text">DEMUESTRA LO QUE APRENDISTE</p>
+            </div>
+        </div>
+    </div>
+
+
+    <a href="">
+        <h2 style="text-align: center">COMENZAR CERTIFICADO</h2>
+    </a>
+    <header>
+        <?php
+            include ("header.html");
+        ?>
+    </header>
+    <?php
+        include ("Footer.html");
+    ?>
+</body>
+
+</html>
---
+++ Pagina de JAVA.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+    <style>
+        h5,p,h3{
+            color: black;
+            text-align: center;
+        }
+        .card{
+            border: solid black 3px;
+            margin: 20px;
+        }
+        body {
+            background: #000000;
+            background: -webkit-linear-gradient(to left, #434343, #000000);
+            background: linear-gradient(to left, #434343, #000000);
+        }
+        img{
+            border: solid #777777 2px;
+        }
+    </style>
+</head>
+<body>
+<div class="card">
+  <div class="card-body">
+    <h5 class="card-title">JAVA</h5>
+    <p class="card-text">EL CERTIFICADO DE JAVA TODAVIA ESTA EN CURSO </p>
+    <h3>GRACIAS POR VISITAR NUESTRA PAGINA</h3>
+  </div>
+  <img src="img/JAVA.jpg" class="card-img-bottom" alt="..." height="350px;">
+</div>
+   <header>
+        <?php
+            include ("header.html");
+        ?>
+    </header>
+    <?php
+        include ("Footer.html");
+    ?>
+    
+</body>
+</html>
