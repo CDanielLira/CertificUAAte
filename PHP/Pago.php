5c4700bf44881b72938c22279578dd11980ae625
Deivis9090 <116929069+Deivis9090@users.noreply.github.com>
2022-10-30 20:21:02 -0600

    Agregado de Codigo de Validacion en Pago...

--- PHP/Pago.php
+++ PHP/Pago.php
@@ -133,6 +133,34 @@ session_start();
         $COD_VER = Filtrado($_POST["CodVer"]);
     }
 
-    
+       
+if (empty($_SESSION["usuario"])) {
+    header("Location: http://localhost/cursophp/CertificUAAte-main/PHP/formlog.php");
+    exit();
+}
+
+
+$file=fopen("../cuentas.txt", "r");
+$band=0; //para saber si la cuenta y contrasena estan en el archivo
+while(!feof($file)){
+    $linea=fgets($file);
+    if ($linea != ""){
+      $aux=preg_split("/[\s,]+/",$linea);   
+      $user = $aux[2];
+     
+      if ($user===$_SESSION["usuario"]){
+          $pago = $aux[4];
+          $band=1; break;
+     }
+    }
+}   
+fclose($file);
+
+if($pago=="false"){
+    header("Location: http://localhost/cursophp/CertificUAAte-main/PHP/Pago.php");
+} 
+else{
+    header("Location: "); #Direccion de examen;
+}
 
 ?>
